cf6684173008347c3f3835f9911f5c46bcbdc6fa
pz0n <hampus.carl.pettersson@gmail.com>
2026-09-16 21:05:44 +0200

    feat: allow force delete of infrastructure
    Allow force delete of terraform infrastructure to allow us to not get billed from Fargate when we are done experimenting

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "payments_api" {
   name                 = "acmecorp/payments-api"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
