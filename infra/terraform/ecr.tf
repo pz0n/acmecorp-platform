05951d4edca555063b04a521c0082c49716403bb
pz0n <hampus.carl.pettersson@gmail.com>
2026-09-16 19:47:11 +0200

    feat: Add ECR repository for payments API

---
+++ infra/terraform/ecr.tf
@@ -0,0 +1,14 @@
+resource "aws_ecr_repository" "payments_api" {
+  name                 = "acmecorp/payments-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Project     = "acmecorp-platform"
+    Environment = "dev"
+    Service     = "payments-api"
+  }
+}
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -18,3 +18,8 @@ output "private_subnet_ids" {
     aws_subnet.private_b.id
   ]
 }
+
+output "payments_api_ecr_url" {
+  description = "ECR repository URL for the payments API"
+  value       = aws_ecr_repository.payments_api.repository_url
+}
